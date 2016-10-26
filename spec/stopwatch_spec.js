5fd4246cbe7da421fae7221450d2242597970519
jsms90 <jsms@hotmail.co.uk>
2016-10-26 23:43:58 +0100

    test whether timer begins incrementing by 1 after each second

--- spec/stopwatch_spec.js
+++ spec/stopwatch_spec.js
@@ -2,4 +2,11 @@ describe("stopwatch", function() {
   it("timer starts at 0", function() {
     expect(timer).toEqual(0);
   });
+
+  it("Count adds 1 to timer after 1.5seconds", function(done) {
+    setTimeout(function() {
+      expect(timer).toEqual(1);
+      done();
+    },1500);
+  });
 });
