105ac82aa7f328e39ae91acfa9356837545d9cf4
jsms90 <jsms@hotmail.co.uk>
2016-10-27 14:46:23 +0100

    test asynchronousity of start function

--- spec/stopwatch_spec.js
+++ spec/stopwatch_spec.js
@@ -30,6 +30,7 @@ QUnit.test( "Timer begins at 0", function( assert ) {
 QUnit.test( "Start function is triggered", function( assert ) {
   start();
   assert.ok( active == true, "Passed!" );
+  clearInterval(intervalId);
 });
 
 // What do you about tests that contradict eachother? Should you not be breaking down this small or is it okay to do them and then comment them out?
@@ -37,15 +38,15 @@ QUnit.test( "Start function is triggered", function( assert ) {
 //   assert.ok( active == false, "Passed!" );
 // });
 
-QUnit.test( "Start adds 1 to timer", function( assert ) {
-  assert.ok( newTimer === 1, "Passed!" );
-});
+// QUnit.test( "Start adds 1 to timer", function( assert ) {
+//   assert.ok( newTimer === 1, "Passed!" );
+// });
 
-QUnit.test( "newTimer totals 3, 3.5seconds after start is called", function( ) {
+QUnit.test( "newTimer totals 3, 3.5seconds after start is called", function(assert) {
   var done = assert.async();
   start();
   setTimeout(function(){
-    ok( timer === 3, "Passed!" );
+    assert.ok(timer === 3, "Passed!" );
     done();
-  }, 3500);
+  }, 3000);
 });
